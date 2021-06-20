6f3ca3e82782904cd8569f5abe422ad61452d4e1
vaidyavadi <86162968+vaidyavadi@users.noreply.github.com>
2021-06-20 12:20:44 +0530

    third commit

---
+++ Blog_Project/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class BlogListView(ListView):
+	model=Post
+	template_name='home.html'
+
+# Create your views here.
